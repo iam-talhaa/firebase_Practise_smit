ed65f1be3401bc42d56d8e8bb176f32fd58e7d22
iam-talhaa <tahakhan4141@gmail.com>
2024-08-31 22:34:14 +0500

    firebase practise silani

---
+++ lib/ui/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              'splash Screen',
+              style: TextStyle(color: Colors.black, fontSize: 40),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
