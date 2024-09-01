e6742ef0dc66659bb3fc6975fefc68b14051250c
iam-talhaa <tahakhan4141@gmail.com>
2024-09-01 22:46:55 +0500

    Login Screen connectivity with firebase

---
+++ lib/ui/auth/login/login.dart
@@ -0,0 +1,86 @@
+import 'package:f_firebase/Custom_widgets/custom_button.dart';
+import 'package:f_firebase/ui/auth/signup/signup.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final _formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        key: _formkey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your Email';
+                  }
+                },
+                keyboardType: TextInputType.emailAddress,
+                controller: emailController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(18)),
+                    prefixIcon: Icon(Icons.lock),
+                    hintText: "Password"),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                },
+                obscureText: true,
+                obscuringCharacter: "#",
+                keyboardType: TextInputType.text,
+                controller: passwordController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(18)),
+                    prefixIcon: Icon(Icons.lock),
+                    hintText: "Password"),
+              ),
+              Row(
+                children: [
+                  Text("Don't Have an Account"),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (BuildContext context) => signUpScreen()));
+                      },
+                      child: Text("Sign Up"))
+                ],
+              ),
+              Custom_button(
+                  B_color: Colors.black,
+                  ontap: () {
+                    print("CHECHEK");
+                  },
+                  B_text: "Login ",
+                  B_height: 45.h,
+                  B_width: 150.h)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/auth/signup/signup.dart
+++ lib/ui/auth/signup/signup.dart
@@ -1,5 +1,6 @@
 import 'package:f_firebase/Custom_widgets/custom_button.dart';
 import 'package:f_firebase/Utils/Utils.dart';
+import 'package:f_firebase/ui/auth/login/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -18,6 +19,21 @@ class _signUpScreenState extends State<signUpScreen> {
   TextEditingController PasswordController = TextEditingController();
   final _formkey = GlobalKey<FormState>();
   FirebaseAuth _auth = FirebaseAuth.instance;
+
+  void Signup() {
+    _auth
+        .createUserWithEmailAndPassword(
+            email: EmailContorller.text.toString().trim(),
+            password: PasswordController.text.toString().trim())
+        .then((v) {
+      UTils().Toastmsg("Login Success Fully");
+    }).onError(
+      (error, stackTrace) {
+        UTils().Toastmsg(error.toString());
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,23 +86,25 @@ class _signUpScreenState extends State<signUpScreen> {
                 B_color: Colors.black,
                 ontap: () {
                   if (_formkey.currentState!.validate()) {
-                    _auth
-                        .createUserWithEmailAndPassword(
-                            email: EmailContorller.text.toString().trim(),
-                            password: PasswordController.text.toString().trim())
-                        .then((v) {
-                      UTils().Toastmsg("Login Success Fully");
-                    }).onError(
-                      (error, stackTrace) {
-                        UTils().Toastmsg(error.toString());
-                      },
-                    );
+                    Signup();
                   }
                 },
                 B_text: 'Sign UP',
                 B_height: 40.h,
                 B_width: 150.w,
-              )
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Already Have an Account"),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (BuildContext context) => LoginScreen()));
+                      },
+                      child: Text("Login"))
+                ],
+              ),
             ],
           ),
         ),
