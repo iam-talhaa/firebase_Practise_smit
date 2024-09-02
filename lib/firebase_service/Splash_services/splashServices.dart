00a51a5db9402de3641eefa11c14aed5bb0bd200
iam-talhaa <tahakhan4141@gmail.com>
2024-09-02 23:24:29 +0500

    home screen added

--- lib/firebase_service/Splash_services/splashServices.dart
+++ lib/firebase_service/Splash_services/splashServices.dart
@@ -1,3 +1,25 @@
+import 'dart:async';
+
+import 'package:f_firebase/ui/auth/login/login.dart';
+import 'package:f_firebase/ui/home/homeScreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class Splashservices {}
+class Splashservices {
+  void Login(BuildContext context) {
+    final _auth = FirebaseAuth.instance;
+    final user = _auth.currentUser;
+
+    if (user != null) {
+      Timer(Duration(seconds: 3), () {
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (BuildContext context) => HomeScreen()));
+      });
+    } else {
+      Timer(Duration(seconds: 3), () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (BuildContext context) => LoginScreen()));
+      });
+    }
+  }
+}
