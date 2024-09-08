d342034b5cb3c7742964b9ae40131fe854af4e2d
iam-talhaa <tahakhan4141@gmail.com>
2024-09-08 23:48:02 +0500

    Firebase Real time data base

--- lib/ui/home/homeScreen.dart
+++ lib/ui/home/homeScreen.dart
@@ -19,27 +19,40 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   FirebaseAuth _auth = FirebaseAuth.instance;
 
-  final FirebaseRef = FirebaseDatabase.instance.ref('counter');
+  final FirebaseRef = FirebaseDatabase.instance.ref('DataBase');
   TextEditingController nameController = TextEditingController();
+  void Delete_Account() {
+    _auth.currentUser!.delete().then((c) {
+      UTils().Toastmsg("User Deleted Successfully", Colors.green);
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (_) => signUpScreen()));
+    }).onError((Error, s) {
+      UTils().Toastmsg(Error.toString(), Colors.red);
+    });
+  }
+
+  void SignOut_Account() {
+    _auth.signOut().then((v) {
+      UTils().Toastmsg("Logout Successfully", Colors.green);
+
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (BuildContext context) => LoginScreen()));
+    }).onError((Error, s) {
+      UTils().Toastmsg(Error.toString(), Colors.red);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    print("object");
-    print("object");
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.lightBlue[200],
         title: Text("Home Screen"),
         centerTitle: true,
         actions: [
           IconButton(
               onPressed: () {
-                _auth.signOut().then((v) {
-                  UTils().Toastmsg("Logout Successfully", Colors.green);
-
-                  Navigator.of(context).pushReplacement(MaterialPageRoute(
-                      builder: (BuildContext context) => LoginScreen()));
-                }).onError((Error, s) {
-                  UTils().Toastmsg(Error.toString(), Colors.red);
-                });
+                SignOut_Account();
               },
               icon: Icon(
                 Icons.logout,
@@ -47,13 +60,7 @@ class _HomeScreenState extends State<HomeScreen> {
               )),
           IconButton(
               onPressed: () {
-                _auth.currentUser!.delete().then((c) {
-                  UTils().Toastmsg("User Deleted Successfully", Colors.green);
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (_) => signUpScreen()));
-                }).onError((Error, s) {
-                  UTils().Toastmsg(Error.toString(), Colors.red);
-                });
+                Delete_Account();
               },
               icon: Icon(
                 Icons.delete,
@@ -62,26 +69,37 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          TextFormField(
-            decoration: InputDecoration(
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-                hintText: "Enter your Name"),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextFormField(
+              style: TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  hintText: "Enter your Name"),
+            ),
+          ),
+          SizedBox(
+            height: 40,
           ),
           Center(
             child: Custom_button(
                 B_color: Colors.black,
                 ontap: () {
-                  final id = DateTime.now().millisecond.toString();
+                  print('Firebase Checking');
+                  final id = DateTime.now().millisecondsSinceEpoch.toString();
+                  print(id);
                   FirebaseRef.child(id).set({
-                    'id': id.toString(),
-                    'Name': nameController.toString().trim(),
-                    'Department': 'Software Engineering'
-                  }).then((v) {
-                    UTils().Toastmsg("ADDED SuccessFull", Colors.green);
-                  }).onError((Error, s) {
-                    UTils().Toastmsg(Error.toString(), Colors.red);
+                    'id': id,
+                    'Name': nameController.text.toString()
+                  }).then((value) {
+                    print('Then');
+                    UTils().Toastmsg('POST ADDDED', Colors.green);
+                  }).onError((error, stackTrace) {
+                    print('ONerror');
+                    UTils().Toastmsg(error.toString(), Colors.red);
                   });
                 },
                 B_text: "Task added",
